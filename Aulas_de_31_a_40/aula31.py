330e41096f9828b0885383c291ee79a2b5c353ea
Taciocaua <tccaua@outlook.com>
2025-10-01 16:01:06 -0300

    aulas organizadas por pastas

--- Aulas_de_31_a_40/aula31.py
+++ Aulas_de_31_a_40/aula31.py
@@ -11,8 +11,8 @@ if condicao:
     passou_no_if = True
     print('Faça algo')
 else:
-    print('Não faça algo')
-    
+    print('Não faça algo') 
+     
 if passou_no_if is None:
     print('Não passou no if')
 else:
