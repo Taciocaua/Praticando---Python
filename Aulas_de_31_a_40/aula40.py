662660c8d1b4413710c0e05b496305225dfd7fbf
Taciocaua <tccaua@outlook.com>
2025-10-06 15:59:12 -0300

    Aulas e praticas de Python

---
+++ Aulas_de_31_a_40/aula40.py
@@ -0,0 +1,13 @@
+"""
+Iterando strings com while
+"""     
+nome = 'Tacio'
+
+indice = 0 
+novo_nome = " "
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f"*{nome}"
+    indice += 1
+  
+print(novo_nome)    
