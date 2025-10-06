a720a0578ef04db5b6f52d1164560e897bd28541
Taciocaua <tccaua@outlook.com>
2025-10-06 13:49:20 -0300

    Aulas e praticas de Python

---
+++ Aulas_de_31_a_40/aula33.py
@@ -0,0 +1,10 @@
+"""
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'Tácio Cauã'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+
+print(string)
+print(outra_variavel)
+
+print(string.zfill(10))
